13e60980f49b7100a52bfa6f8edfb68ea9683258
kusaLLL <kusalabeysinghe@gmail.com>
2023-06-21 22:49:01 +0530

    class 4

---
+++ script.js
@@ -0,0 +1,8 @@
+function f1(){
+    document.getElementById("d1").style.marginLeft = "500px" 
+    document.getElementById("d1").style.backgroundColor = "black" ;
+    document.getElementById("d1").style.color = "white" ;
+    document.getElementById("d1").innerHTML = "500px" ;
+
+
+}
